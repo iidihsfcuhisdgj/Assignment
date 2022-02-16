8f7b01e013d56fc98aa32cb8777b3ffaa6892e35
Irfan <s19bsit007@student.ug.edu.pk>
2022-02-16 10:57:34 +0500

    about page

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ const app = express()
 app.get("/", (req, res) => {
     res.json("Home page")
 })
+app.get("/about", (req, res) => {
+    res.json("about page")
+})
+
 
 app.listen(3000, () => console.log("Server is running on port 3000"))
